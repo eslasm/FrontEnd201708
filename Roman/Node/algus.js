6e005bde15c6be0f5fdc8f57aa2f12743f4ebf08
Roman Gorislavski <roman.goris@hotmail.com>
2017-08-17 14:34:40 +0300

    Tingimuslaused ja tsüklid

--- Roman/Node/algus.js
+++ Roman/Node/algus.js
@@ -38,10 +38,65 @@ console.log(arv);
 arv++; // 3
 ++arv; // 4
 arv--; // 3
-arv = 5 * (3 + 4); // 35
+arv = (3 + 4) * 5; // 35
 arv = Math.pow(10, 2); // 100 astmes eksperimentaalne
 console.log(arv);
 s6ne += ' sõne';
 s6ne += " arv: " + arv + ".";
+console.log(s6ne);
+/* tingimuslaused */
+s6ne = arv === 100 ? 'Tere' : 'Headaega';
+console.log(s6ne);
+var a = 10;
+if (arv === 100) {
+    var a_1 = 0; // potentsiaalne shadowed variable
+    s6ne = 'Tere';
+}
+else if (arv < 100) {
+    s6ne = 'Headaega';
+}
+else {
+    s6ne = '...';
+}
+console.log(a);
+switch (arv) {
+    case 100:
+        s6ne = 'Tere';
+        break;
+    case 102:
+    case 90:
+        s6ne = 'Headaega';
+        break;
+    default:
+        s6ne = '...';
+}
+switch (v2rv) {
+    case 1:
+    case V2rv.Punane:
+        s6ne = 'Roheline või Punane';
+        break;
+    default:
+        s6ne = 'Muu värv';
+}
+if (s6ne !== 'Tere' && arv >= 0) {
+    s6ne = 'Tingimus täidetud';
+}
+// === on täpsem kui ==  sama ka !== !=
+/* Tsükklid */
+for (var i = 0; i < 5; ++i) {
+    if (i === 3) {
+        continue;
+    }
+    console.log("V\u00E4\u00E4rtus ts\u00FCklis " + i);
+}
+arv = 0;
+while (arv <= 10) {
+    s6ne += '2';
+    if (arv === 5) {
+        s6ne += '3';
+        break;
+    }
+    arv++;
+}
 console.log(s6ne);
 //# sourceMappingURL=algus.js.map
--- Roman/Node/algus.ts
+++ Roman/Node/algus.ts
@@ -39,10 +39,68 @@ console.log(arv);
 arv++; // 3
 ++arv; // 4
 arv--; // 3
-arv = 5 * (3 + 4); // 35
+arv = (3 + 4) * 5; // 35
 arv = 10 ** 2; // 100 astmes eksperimentaalne
 console.log(arv);
 
 s6ne += ' sõne';
 s6ne += ` arv: ${arv}.`;
 console.log(s6ne);
+
+/* tingimuslaused */
+s6ne = arv === 100 ? 'Tere' : 'Headaega';
+console.log(s6ne);
+let a = 10;
+if (arv === 100) {
+    let a = 0; // potentsiaalne shadowed variable
+    s6ne = 'Tere';
+} else if (arv < 100) {
+    s6ne = 'Headaega';
+} else {
+    s6ne = '...';
+}
+console.log(a);
+
+switch (arv) {
+case 100:
+    s6ne = 'Tere';
+    break;
+case 102:
+case 90:
+    s6ne = 'Headaega';
+    break;
+default:
+    s6ne = '...';
+}
+switch (v2rv) {
+case 1:
+case V2rv.Punane:
+    s6ne = 'Roheline või Punane';
+    break;
+default:
+    s6ne = 'Muu värv';
+}
+
+if (s6ne !== 'Tere' && arv >= 0) { // &&-ja, ||-v6i
+    s6ne = 'Tingimus täidetud';
+}
+// === on täpsem kui ==  sama ka !== !=
+
+/* Tsükklid */
+for (let i = 0; i < 5; ++i) {
+    if (i === 3) { continue; }
+    console.log(`Väärtus tsüklis ${i}`);
+}
+arv = 0;
+while (arv <= 10) {
+    s6ne += '2';
+    if (arv === 5) {
+        s6ne += '3';
+        break;
+    }
+    arv++;
+}
+console.log(s6ne);
+// do while
+
+/* funktsioon */
